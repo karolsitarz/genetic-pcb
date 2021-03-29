d4d08a4d18016446c0d0780cb9006667dc932c59
Karol Sitarz <ka.sitarz@gmail.com>
2021-03-29 06:04:33 +0200

    add tailwind safelist

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,16 @@
 module.exports = {
-  purge: ["./src/**/*.tsx"],
+  purge: {
+    content: ["./src/**/*.tsx"],
+    options: {
+      safelist: {
+        standard: [
+          /^bg-(red|yellow|green|blue|purple|pink)-(300|500|900)$/,
+          /^ring-(red|yellow|green|blue|purple|pink)-100$/,
+          /^bg-gray-(300|600)$/,
+        ],
+      },
+    },
+  },
   darkMode: "media",
   theme: {
     extend: {},
